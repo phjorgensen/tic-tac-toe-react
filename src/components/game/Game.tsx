8c1d00357c1571b853c4c5aa7669f15b6bcafe94
Per Henrik <phjorgensenn@gmail.com>
2021-02-11 19:45:25 +0100

    Added RoundInfo component

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Board } from '../board/Board';
+// import { RoundInfo } from '../round-info/RoundInfo';
 import './Game.css';
 
 export class Game extends React.Component {
     render() {
         return (
             <div className="game">
+                {/* <div>
+                    <RoundInfo />
+                </div> */}
+
                 <div className="game-board">
                     <Board />
                 </div>
+
                 <div className="game-info">
                     <div>{/* status */}</div>
                     <ol>{/* TODO */}</ol>
---
+++ src/components/round-info/RoundInfo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './RoundInfo.css';
+
+export class RoundInfo extends React.Component {
+    render() {
+        return (
+            <div>Test</div>
+        );
+    }
+}
