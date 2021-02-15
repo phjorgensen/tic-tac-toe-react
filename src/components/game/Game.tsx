3f5b8205adb4ab3cffa41676a7894a4325429526
Per Henrik <phjorgensenn@gmail.com>
2021-02-15 20:17:08 +0100

    Displaying the game history in the Game component

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BoardState } from '../../models/BoardState';
 import { Board } from '../board/Board';
+import { GameHistory } from '../game-history/GameHistory';
 import { RoundInfo } from '../round-info/RoundInfo';
 import './Game.css';
 
@@ -10,6 +11,7 @@ interface GameProps { }
 interface GameState {
     history: BoardState[];
     xIsNext: boolean;
+    stepIndex: number;
     win: boolean;
 }
 
@@ -26,38 +28,43 @@ export class Game extends React.Component<GameProps, GameState> {
                 },
             ],
             xIsNext: true,
+            stepIndex: 0,
             win: false,
         };
     }
 
     private handleClick(i: number): void {
-        if (this.state.win) {
-            return;
-        }
-
-        const current = this.getCurrentBoardState();
-
-        if (current.squares[i]) {
-            return;
-        }
-
+        const history = this.state.history.slice(0, this.state.stepIndex + 1);
+        const current = history[history.length - 1];
         const squares = current.squares.slice();
+
+        if (this.state.win || squares[i]) {
+            return;
+        }
 
         squares[i] = this.state.xIsNext ? 'X' : 'O';
 
         this.setState({
-            history: this.state.history.concat([{ squares }]),
+            history: history.concat([{ squares }]),
+            stepIndex: history.length,
             xIsNext: !this.state.xIsNext,
         });
     }
 
-    private handleWin() {
+    private handleWin(): void {
         this.setState({
             win: true,
         });
     }
 
-    private handleReset() {
+    private handleJump(stepIndex: number, xIsNext: boolean): void {
+        this.setState({
+            stepIndex,
+            xIsNext,
+        });
+    }
+
+    private handleReset(): void {
         this.setState({
             history: [
                 {
@@ -65,23 +72,22 @@ export class Game extends React.Component<GameProps, GameState> {
                 },
             ],
             xIsNext: true,
+            stepIndex: 0,
             win: false,
         });
     }
 
-    private getCurrentBoardState(): BoardState {
+    public render(): JSX.Element {
         const history = this.state.history;
-
-        return history[history.length - 1]
-    }
-
-    render() {
-        const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepIndex];
 
         return (
             <div>
                 <div className="game">
+                    <div>
+                        <GameHistory history={history} onJump={(step, xIsNext) => this.handleJump(step, xIsNext)} />
+                    </div>
+
                     <div>
                         <RoundInfo boardState={current} xIsNext={this.state.xIsNext} onWin={() => this.handleWin()} onReset={() => this.handleReset()} />
                     </div>
